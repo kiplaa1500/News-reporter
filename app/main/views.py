3e510c782f05c96eb132a83688c62e29ade5d0b7
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-06-06 20:01:00 +0300

    Adding a funtion to render news upon search

--- app/main/views.py
+++ app/main/views.py
@@ -12,7 +12,7 @@ def index():
     all_news_sources = get_all_news_sources()
     everything_news_items = get_everything_news()
     business_headliness = get_business_headlines()
-    title = "Giko"
+    title = "News"
     search_article = request.args.get('article_query')
     if search_article:
         return redirect(url_for('.search', source_name=search_article))
@@ -20,3 +20,23 @@ def index():
         return render_template("index.html", sources=all_news_sources, title=title, others=everything_news_items, business_headliness=business_headliness)
 
 
+@main.route('/source/<source>')
+def news_healines(source):
+    """
+    This function retrieves live top and breaking headlines for a country.
+    """
+    title = "News headlines"
+    news_healines = get_all_news_headlines(source)
+    return render_template('news_articles.html', headlines=news_healines, title=title)
+
+
+@main.route('/search/<source_name>')
+def search(source_name):
+    """
+    View function to display search results.
+    """
+    source_name_list = source_name.split(" ")
+    source_name_format = "+".join(source_name_list)
+    searched_sources = search_articles(source_name_format)
+    title = f'search results for {source_name}'
+    return render_template('search.html', results=searched_sources)
